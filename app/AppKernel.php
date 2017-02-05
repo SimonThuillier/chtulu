e2d7b8426a13a08323cfb35b5a1cdbe40844bf2b
Simon Thuillier <sthuillier1988@gmail.com>
2017-02-05 20:27:25 +0100

    ajout de tinymce et premiers tests ( c'est gros et grand ! )

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new PlatformBundle\PlatformBundle(),
+            new ArticleBundle\ArticleBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
