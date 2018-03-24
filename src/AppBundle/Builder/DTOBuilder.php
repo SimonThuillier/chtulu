54e72370a3ec710f7aa58bdd7c7b2e72b78d0aca
Belze88 <sthuillier1988@gmail.com>
2018-03-24 02:02:56 +0100

    avancée articleDTO et articleBuilder

---
+++ src/AppBundle/Builder/DTOBuilder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ajeelomen-1
+ * Date: 24/03/18
+ * Time: 00:49
+ */
+
+namespace AppBundle\Builder;
+
+
+class DTOBuilder
+{
+
+}
---
+++ src/AppBundle/Builder/NullDTOException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ajeelomen-1
+ * Date: 24/03/18
+ * Time: 01:15
+ */
+
+namespace AppBundle\Builder;
+
+
+class NotAvailablePartException extends \Exception
+{
+
+}
